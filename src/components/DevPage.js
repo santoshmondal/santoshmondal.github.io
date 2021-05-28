03b8b9b0f7117c31c8dc5ad591e95b0aaeeddd3a
santosh <santosh.ece06@gmail.com>
2021-05-29 05:25:04 +0530

    dev setup.

--- src/components/DevPage.js
+++ src/components/DevPage.js
@@ -3,7 +3,7 @@ import "./IndexPage.css";
 export const DevPage = () => {
   return (
     <div className="beautify">
-      <div style={{ fontSize: "40px" }}>Dev and Testing</div>
+      <div>Dev and Testing</div>
       <div>Github Profile</div>
     </div>
   );
