63090b9c5023fe5d6ab1a55b1b2107ec5955f17a
santosh <santosh.ece06@gmail.com>
2021-05-28 20:02:34 +0530

    routing...continue.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router";
+import { ComingSoon } from "./components/ComingSoon";
 import { IndexPage } from "./components/IndexPage";
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={IndexPage} />
+      <Route exact path="/dev" component={ComingSoon} />
     </Switch>
   );
 }
---
+++ src/components/ComingSoon.js
@@ -0,0 +1,10 @@
+import "./IndexPage.css";
+
+export const ComingSoon = () => {
+  return (
+    <div className="beautify">
+      <div style={{ fontSize: "40px" }}>Coming Soon!</div>
+      <div>Github Profile</div>
+    </div>
+  );
+};
