0cfb70d3b9ad3abeef5b474007463a9ae980198d
santosh <santosh.ece06@gmail.com>
2021-05-29 07:24:15 +0530

    material ui lab added.

--- src/components/DevPage.js
+++ src/components/DevPage.js
@@ -1,9 +1,43 @@
+import { Box, makeStyles, useTheme } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 import "./IndexPage.css";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    color: theme.palette.primary.main,
+  },
+}));
+
 export const DevPage = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  console.log(theme);
+
   return (
     <div className="beautify">
-      <div>Dev and Testing</div>
+      <Box className={classes.root} p={2} borderRadius={4} fontWeight="bolder">
+        Dev And Testing
+      </Box>
+      <Box mb={1}>
+        <Alert severity="info">This is an info alert — check it out!</Alert>
+      </Box>
+
+      <Box mb={1}>
+        <Alert severity="success">
+          This is an success alert — check it out!
+        </Alert>
+      </Box>
+
+      <Box mb={1}>
+        <Alert severity="warning">
+          This is an warning alert — check it out!
+        </Alert>
+      </Box>
+
+      <Box mb={1}>
+        <Alert severity="error">This is an error alert — check it out!</Alert>
+      </Box>
+
       <div>Github Profile</div>
     </div>
   );
--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,17 +1,23 @@
 import { useSelector } from "react-redux";
 import "./IndexPage.css";
 import Button from "@material-ui/core/Button";
+import { Box } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 
 export const IndexPage = () => {
   const appState = useSelector((state) => state);
 
   return (
     <div className="beautify">
-      <div>{appState.title}</div>
-      <div>{appState.message}</div>
+      <Box mb>
+        <Alert severity="info">{appState.title}</Alert>
+      </Box>
+      <Box mb>
+        <Alert severity="success">{appState.message}</Alert>
+      </Box>
 
-      <Button variant="outlined" color="primary">
-        Material Button
+      <Button variant="outlined" size="small">
+        Check Out
       </Button>
     </div>
   );
