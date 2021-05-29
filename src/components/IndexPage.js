0e866d31dac42d913ae866382bf00faa45e8c4e0
santosh <santosh.ece06@gmail.com>
2021-05-29 14:01:57 +0530

    title bar.

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,13 +1,24 @@
 import { useSelector } from "react-redux";
 import "./IndexPage.css";
 import Button from "@material-ui/core/Button";
-import { Box, Grid, Icon, Typography, useTheme } from "@material-ui/core";
+import {
+  Box,
+  Grid,
+  Icon,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 import SendIcon from "@material-ui/icons/Send";
 
 export const IndexPage = () => {
-  const appState = useSelector((state) => state);
   const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+
+  console.log(theme);
+
+  const appState = useSelector((state) => state);
 
   return (
     <Box display="flex" flexDirection="column" style={{ height: "100vh" }}>
@@ -19,13 +30,20 @@ export const IndexPage = () => {
         justifyContent="center"
         height={1 / 6}
         style={{ backgroundColor: "#333330" }}
+        p={{ xs: 1, md: 0 }}
       >
-        <Typography>
-          <Box>Santosh Mondal</Box>
-        </Typography>
-        <Typography>
-          <Box>FOUNDER / PRODUCT ARCHITECT</Box>
-        </Typography>
+        <Box textAlign="center">
+          <Typography
+            variant={matches ? "h4" : "h5"}
+            style={{ fontWeight: "bolder" }}
+          >
+            Santosh Kumar Mondal
+          </Typography>
+        </Box>
+
+        <Box textAlign="center">
+          <Typography variant="h6">Founder / Product Architect</Typography>
+        </Box>
       </Box>
 
       <Grid container>
