81c0add460536a74f60e8dcee5337f7599c2c683
santosh <santosh.ece06@gmail.com>
2021-05-29 14:12:19 +0530

    using appbar.

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,54 +1,58 @@
 import { useSelector } from "react-redux";
 import "./IndexPage.css";
-import Button from "@material-ui/core/Button";
 import {
+  AppBar,
   Box,
   Grid,
-  Icon,
+  makeStyles,
+  Toolbar,
   Typography,
   useMediaQuery,
   useTheme,
 } from "@material-ui/core";
-import { Alert } from "@material-ui/lab";
-import SendIcon from "@material-ui/icons/Send";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    color: theme.palette.primary.main,
+  },
+  toolbar: {
+    height: "128px",
+    backgroundColor: "#333330",
+
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+}));
 
 export const IndexPage = () => {
+  const classes = useStyles();
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up("sm"));
-
   console.log(theme);
-
   const appState = useSelector((state) => state);
 
   return (
-    <Box display="flex" flexDirection="column" style={{ height: "100vh" }}>
-      <Box
-        color={theme.palette.common.white}
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        height={1 / 6}
-        style={{ backgroundColor: "#333330" }}
-        p={{ xs: 1, md: 0 }}
-      >
-        <Box textAlign="center">
-          <Typography
-            variant={matches ? "h4" : "h5"}
-            style={{ fontWeight: "bolder" }}
-          >
-            Santosh Kumar Mondal
-          </Typography>
-        </Box>
+    <>
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <Box textAlign="center">
+            <Typography
+              variant={matches ? "h4" : "h5"}
+              style={{ fontWeight: "inherit" }}
+            >
+              Santosh Kumar Mondal
+            </Typography>
+          </Box>
 
-        <Box textAlign="center">
-          <Typography variant="h6">Founder / Product Architect</Typography>
-        </Box>
-      </Box>
-
-      <Grid container>
-        <Grid item>Container 2</Grid>
-      </Grid>
-    </Box>
+          <Box textAlign="center">
+            <Typography variant={matches ? "h6" : "body1"}>
+              Founder / Product Architect
+            </Typography>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
