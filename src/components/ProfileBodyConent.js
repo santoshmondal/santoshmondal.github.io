7067ec2bc47a29849fb209db6ede1e51609cd7cc
santosh <santosh.ece06@gmail.com>
2021-05-30 12:59:04 +0530

    profile body.

--- src/components/ProfileBodyConent.js
+++ src/components/ProfileBodyConent.js
@@ -1,11 +1,29 @@
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Box,
-  Paper,
+  makeStyles,
   Typography,
   useMediaQuery,
   useTheme,
 } from "@material-ui/core";
 import { useSelector } from "react-redux";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: "33.33%",
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+  },
+}));
 
 export const ProfileSumaryContent = () => {
   const theme = useTheme();
@@ -31,15 +49,47 @@ export const ProfileSumaryContent = () => {
 
 export const ProfileBodyContent = () => {
   const theme = useTheme();
+  const classes = useStyles();
   const matches = useMediaQuery(theme.breakpoints.up("sm"));
-  const appState = useSelector((state) => state);
   const employment = useSelector((state) => state.profile.employment);
 
   return (
     <Box p={1} px={2} pb={1}>
-      <Box color="primary.contrastText" fontWeight="bold">
+      <Box color="primary.contrastText" fontWeight="bold" mb={1}>
         <Typography variant="h6">{employment.title}</Typography>
       </Box>
+
+      {[1, 2, 3, 4].map((item, index) => (
+        <Accordion
+          key={index}
+          style={{ background: "#3C3C39", color: theme.palette.common.white }}
+        >
+          <AccordionSummary
+            expandIcon={
+              <ExpandMoreIcon style={{ color: theme.palette.common.white }} />
+            }
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className={classes.heading}>
+              General settings
+            </Typography>
+            <Typography
+              className={classes.secondaryHeading}
+              style={{ color: theme.palette.grey[400] }}
+            >
+              I am an accordion
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </Box>
   );
 };
