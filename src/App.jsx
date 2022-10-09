f768ac8ee150a74f08b1298d69b2d9f6c524d8a2
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-10-09 09:15:33 +0530

    mui setup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { AppBar, Toolbar } from "@mui/material";
+
 function App() {
   return (
     <div>
+      <AppBar>
+        <Toolbar></Toolbar>
+      </AppBar>
       <h1>Hello World</h1>
     </div>
   );
