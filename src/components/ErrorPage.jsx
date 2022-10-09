39611ab482b0ebff2d53eafe9c2d90145d629f37
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-10-09 18:14:39 +0530

    error page. ...

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,16 +1,51 @@
-import { useRouteError } from "react-router-dom";
+import { Button, Paper, Typography } from "@mui/material";
+import { Box, Container } from "@mui/system";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
   const error = useRouteError();
+  let navigate = useNavigate();
+
+  const toHomePage = () => {
+    navigate("/");
+  };
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <Box
+      sx={{ height: "100vh" }}
+      display="flex"
+      flexDirection={"column"}
+      justifyContent="center"
+      alignItems={"center"}
+    >
+      <Paper id="error-page">
+        <Box
+          sx={{ width: "50vw", p: 4 }}
+          display="flex"
+          flexDirection={"column"}
+          justifyContent="center"
+          alignItems={"center"}
+        >
+          <Typography variant="h1" textAlign={"center"}>
+            Oops!
+          </Typography>
+          <Typography variant="h5" textAlign={"center"}>
+            Sorry, an unexpected error has occurred.
+          </Typography>
+          <Typography textAlign={"center"}>
+            <i>{error.statusText || error.message}</i>
+          </Typography>
+
+          <Button
+            variant="contained"
+            sx={{ mt: 2 }}
+            onClick={() => navigate("/")}
+          >
+            Return Back
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
   );
 };
 
